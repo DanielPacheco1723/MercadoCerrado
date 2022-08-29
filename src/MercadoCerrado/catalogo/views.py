f53b1b22cb141b66371c3ca6978a4ae9792cde83
Edward Ap Iwan <edwapiwan@gmail.com>
2022-08-29 11:58:07 -0300

    modified:   catalogo/__pycache__/urls.cpython-39.pyc modified:   catalogo/__pycache__/views.cpython-39.pyc modified:   catalogo/templates/login.html modified:   catalogo/templates/logout.html modified:   catalogo/templates/registro.html modified:   catalogo/views.py

--- src/MercadoCerrado/catalogo/views.py
+++ src/MercadoCerrado/catalogo/views.py
@@ -3,14 +3,14 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Producto
-from catalogo import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login,logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.forms import NullBooleanField
 from django.shortcuts import redirect, render, resolve_url
 from django.http import HttpResponse
+from .models import Producto
+from .forms import UserRegisterForm
 
 class ListaProductos (ListView):
     model = Producto
@@ -50,3 +50,59 @@ def register(request):
       else:
             form = UserRegisterForm()
       return render(request, "registro.html", {"form": form})    
+
+def login_request(request):
+      
+      
+      if request.method == "POST":
+            form = AuthenticationForm(request, data = request.POST)
+            
+            if form.is_valid():
+                  usuario = form.cleaned_data.get('username')
+                  contra = form.cleaned_data.get('password')
+                  
+                  user = authenticate(username=usuario, password = contra)
+                  
+                  
+                  if user is not None:
+                        login(request, user)
+                        avatares = Avatar.objects.filter(user=request.user.id).exists()
+                        if avatares == True:
+                              avatares = Avatar.objects.filter(user=request.user.id)
+                              print(avatares)
+                              return render(request, "base.html", {"url": avatares[0].imagen.url})
+                        #return render(request, "/base.html", {"mensaje": f"Bienvenido {usuario}"})
+                        else:
+                              return render(request, "/base.html", {"mensaje": f"Bienvenido {usuario}"})
+                  else:
+                        return render(request, "error.html", {"mensaje": f"Error, datos incorrectos"})
+            
+            else:
+                  return render(request,"/error.html", {"mensaje": f"Error, formulario equivocado"})      
+      
+      form = AuthenticationForm()
+      return render(request, "login.html", {"form": form})        
+      
+      
+      if request.method == "POST":
+            form = AuthenticationForm(request, data = request.POST)
+            
+            if form.is_valid():
+                  usuario = form.cleaned_data.get('username')
+                  contra = form.cleaned_data.get('password')
+                  
+                  user = authenticate(username=usuario, password = contra)
+                  
+                  if user is not None:
+                        login(request, user)
+                        avatares = Avatar.objects.filter(user=request.user.id)
+                        return render(request, "/base.html", {"url": avatares[0].imagen.url})
+                        #return render(request, "/base.html", {"mensaje": f"Bienvenido {usuario}"})
+                  else:
+                        return render(request, "/error.html", {"mensaje": f"Error, datos incorrectos"})
+            
+            else:
+                  return render(request,"/error.html", {"mensaje": f"Error, formulario equivocado"})      
+      
+      form = AuthenticationForm()
+      return render(request, "/login.html", {"form": form})   
