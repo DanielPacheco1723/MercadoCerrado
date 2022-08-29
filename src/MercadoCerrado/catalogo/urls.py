5f803eed5d3c414d7aa4e4588e4ed1fbfda58bb2
Edward Ap Iwan <edwapiwan@gmail.com>
2022-08-29 02:12:57 -0300

    modified:   MercadoCerrado/__pycache__/__init__.cpython-39.pyc 	modified:   MercadoCerrado/__pycache__/settings.cpython-39.pyc 	modified:   MercadoCerrado/__pycache__/urls.cpython-39.pyc 	modified:   MercadoCerrado/__pycache__/wsgi.cpython-39.pyc 	modified:   catalogo/__pycache__/__init__.cpython-39.pyc 	modified:   catalogo/__pycache__/admin.cpython-39.pyc 	modified:   catalogo/__pycache__/apps.cpython-39.pyc 	new file:   catalogo/__pycache__/forms.cpython-39.pyc 	modified:   catalogo/__pycache__/models.cpython-39.pyc 	modified:   catalogo/__pycache__/urls.cpython-39.pyc 	modified:   catalogo/__pycache__/views.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0001_initial.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0002_alter_producto_descripcion_alter_producto_nombre_and_more.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0003_alter_producto_descripcion.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0004_alter_producto_descripcion.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0005_alter_producto_descripcion.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0006_producto_precio_currency_alter_producto_precio.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0007_alter_producto_descripcion.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/0008_alter_producto_descripcion.cpython-39.pyc 	modified:   catalogo/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   catalogo/templates/login.html 	new file:   catalogo/templates/logout.html 	new file:   catalogo/templates/registro.html 	modified:   catalogo/urls.py 	modified:   catalogo/views.py

--- src/MercadoCerrado/catalogo/urls.py
+++ src/MercadoCerrado/catalogo/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import DetalleProductos, ListaProductos, AgregarProducto, EditarProducto, EliminarProducto
-
+from django.contrib.auth.views import LogoutView
+from catalogo import views
 urlpatterns = [
     path ('', ListaProductos.as_view(), name = 'catalogo'),
     path ('producto/<int:pk>', DetalleProductos.as_view(), name = 'descripcion_producto'),
     path ('agregar-producto/', AgregarProducto.as_view(), name = 'form-producto'),
     path ('editar-producto/<int:pk>', EditarProducto.as_view(), name = 'editar_producto'),
-    path ('eliminar-producto/<int:pk>', EliminarProducto.as_view(), name = 'eliminar_producto')
-
+    path ('eliminar-producto/<int:pk>', EliminarProducto.as_view(), name = 'eliminar_producto'),
+    path('login', views.login_request, name = 'Login'),
+    path('register', views.register, name = 'Register'),
+    path('logout', LogoutView.as_view(template_name='MCD1/logout.html'), name="logout"),
 
 ]
--- src/MercadoCerrado/catalogo/views.py
+++ src/MercadoCerrado/catalogo/views.py
@@ -4,7 +4,13 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Producto
-
+from catalogo import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login,logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.forms import NullBooleanField
+from django.shortcuts import redirect, render, resolve_url
+from django.http import HttpResponse
 
 class ListaProductos (ListView):
     model = Producto
@@ -33,3 +39,14 @@ class EliminarProducto (DeleteView):
     template_name = "eliminar_producto.html"
     context_object_name = "producto"
     success_url = reverse_lazy("catalogo")
+
+def register(request):
+      form = UserRegisterForm(request.POST)
+      if form.is_valid():
+            username = form.cleaned_data['username']
+            first_name=form.cleaned_data['first_name']
+            form.save()
+            return render(request, "MCD1/correcto.html", {"mensaje":"Usuario Creado!"})
+      else:
+            form = UserRegisterForm()
+      return render(request, "MCD1/registro.html", {"form": form})    
