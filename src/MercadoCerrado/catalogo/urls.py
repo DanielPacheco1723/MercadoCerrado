344b359dec6911bc12336b1a7ddb7904a25b80bb
Edward Ap Iwan <edwapiwan@gmail.com>
2022-08-30 23:26:40 -0300

    new file:   accounts/__init__.py new file:   accounts/__pycache__/__init__.cpython-39.pyc new file:   accounts/__pycache__/views.cpython-39.pyc new file:   accounts/apps.py new file:   accounts/migrations/__init__.py new file:   accounts/views.py modified:   catalogo/__pycache__/forms.cpython-39.pyc modified:   catalogo/__pycache__/models.cpython-39.pyc modified:   catalogo/__pycache__/urls.cpython-39.pyc modified:   catalogo/__pycache__/views.cpython-39.pyc deleted:    catalogo/forms.py new file:   catalogo/templates/accounts/login.html new file:   catalogo/templates/accounts/logout.html new file:   catalogo/templates/accounts/register.html deleted:    catalogo/templates/login.html deleted:    catalogo/templates/logout.html deleted:    catalogo/templates/registro.html deleted:    catalogo/tests.py modified:   catalogo/urls.py modified:   catalogo/views.py modified:   db.sqlite3

--- src/MercadoCerrado/catalogo/urls.py
+++ src/MercadoCerrado/catalogo/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
+from django.urls import include, path, re_path
 from .views import DetalleProductos, ListaProductos, AgregarProducto, EditarProducto, EliminarProducto
-from django.contrib.auth.views import LogoutView
-from catalogo import views
+from accounts.views import login_view,logout_view,register_view
 
 urlpatterns = [
     path ('', ListaProductos.as_view(), name = 'catalogo'),
@@ -9,8 +8,7 @@ urlpatterns = [
     path ('agregar-producto/', AgregarProducto.as_view(), name = 'form-producto'),
     path ('editar-producto/<int:pk>', EditarProducto.as_view(), name = 'editar_producto'),
     path ('eliminar-producto/<int:pk>', EliminarProducto.as_view(), name = 'eliminar_producto'),
-    path('login', views.login_request, name = 'Login'),
-    path('register', views.register, name = 'Register'),
-    path('logout', LogoutView.as_view(template_name='MCD1/logout.html'), name="logout"),
-
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view)
 ]
--- src/MercadoCerrado/catalogo/views.py
+++ src/MercadoCerrado/catalogo/views.py
@@ -4,13 +4,16 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login,logout, authenticate
+
 from django.contrib.auth.decorators import login_required
 from django.forms import NullBooleanField
 from django.shortcuts import redirect, render, resolve_url
 from django.http import HttpResponse
 from .models import Producto
-from .forms import UserRegisterForm
+
+from django.shortcuts import render, redirect
+
+
 
 class ListaProductos (ListView):
     model = Producto
@@ -39,70 +42,3 @@ class EliminarProducto (DeleteView):
     template_name = "eliminar_producto.html"
     context_object_name = "producto"
     success_url = reverse_lazy("catalogo")
-
-def register(request):
-      form = UserRegisterForm(request.POST)
-      if form.is_valid():
-            username = form.cleaned_data['username']
-            first_name=form.cleaned_data['first_name']
-            form.save()
-            return render(request, "index.html", {"mensaje":"Usuario Creado!"})
-      else:
-            form = UserRegisterForm()
-      return render(request, "registro.html", {"form": form})    
-
-def login_request(request):
-      
-      
-      if request.method == "POST":
-            form = AuthenticationForm(request, data = request.POST)
-            
-            if form.is_valid():
-                  usuario = form.cleaned_data.get('username')
-                  contra = form.cleaned_data.get('password')
-                  
-                  user = authenticate(username=usuario, password = contra)
-                  
-                  
-                  if user is not None:
-                        login(request, user)
-                        avatares = Avatar.objects.filter(user=request.user.id).exists()
-                        if avatares == True:
-                              avatares = Avatar.objects.filter(user=request.user.id)
-                              print(avatares)
-                              return render(request, "base.html", {"url": avatares[0].imagen.url})
-                        #return render(request, "/base.html", {"mensaje": f"Bienvenido {usuario}"})
-                        else:
-                              return render(request, "/base.html", {"mensaje": f"Bienvenido {usuario}"})
-                  else:
-                        return render(request, "error.html", {"mensaje": f"Error, datos incorrectos"})
-            
-            else:
-                  return render(request,"/error.html", {"mensaje": f"Error, formulario equivocado"})      
-      
-      form = AuthenticationForm()
-      return render(request, "login.html", {"form": form})        
-      
-      
-      if request.method == "POST":
-            form = AuthenticationForm(request, data = request.POST)
-            
-            if form.is_valid():
-                  usuario = form.cleaned_data.get('username')
-                  contra = form.cleaned_data.get('password')
-                  
-                  user = authenticate(username=usuario, password = contra)
-                  
-                  if user is not None:
-                        login(request, user)
-                        avatares = Avatar.objects.filter(user=request.user.id)
-                        return render(request, "/base.html", {"url": avatares[0].imagen.url})
-                        #return render(request, "/base.html", {"mensaje": f"Bienvenido {usuario}"})
-                  else:
-                        return render(request, "/error.html", {"mensaje": f"Error, datos incorrectos"})
-            
-            else:
-                  return render(request,"/error.html", {"mensaje": f"Error, formulario equivocado"})      
-      
-      form = AuthenticationForm()
-      return render(request, "/login.html", {"form": form})   
